1c4e08e3aa7b328cddec7377e927c3e745c1065f
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-01 15:54:36 +0100

    Inizio esercizio Email

---
+++ Mail/js/script.js
@@ -0,0 +1,17 @@
+const emails = []
+emails.push("jimoyoh589@alibrs.com")
+emails.push("dab1a165bbad96@cashbenties.com")
+emails.push("53695265bbada1@crankymonkey.info")
+emails.push("2a5a3c65bbada8@crankymonkey.info")
+emails.push("c3bef965bbadb0@cashbenties.com")
+emails.push("206ad665bbadb8@cashbenties.com")
+emails.push("72841c65bbadc1@cashbenties.com")
+emails.push("ea8c9f65bbadc9@cashbenties.com")
+emails.push("94cb1165bbadd1@cashbenties.com")
+emails.push("e5dd0965bbade1@cashbenties.com")
+emails.push("08447665bbade9@crankymonkey.info")
+console.log(emails)
+
+for (let i = 0; i < emails.lenght; i++ ){
+    console.log(emails[i])
+}
