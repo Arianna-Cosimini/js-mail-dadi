74d011b7fef3e2b404f7a6e88f6385b662a9da48
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-01 15:22:24 +0100

    Fine gioco dei dadi.

--- Gioco Dadi/js/script.js
+++ Gioco Dadi/js/script.js
@@ -1,1 +1,37 @@
-const userChoice = Math.floor(Math.random() * 6) + 1;
+
+
+let myButton = document.getElementById("my-button");
+myButton.addEventListener("click",
+    function () {
+
+        
+        let userChoice = ""
+        let result = document.querySelector(".result");
+
+        userChoice = document.getElementById("choice-User").value;
+        console.log(userChoice)
+        
+
+        const computerChoice = Math.floor(Math.random() * 7) + 1;
+        console.log(computerChoice)
+
+        if (userChoice < 1 || userChoice > 6){
+            result.innerHTML="Attenzione! Inserire solo numeri da 1 a 6!"
+        }
+
+        else if (userChoice == computerChoice) {
+            result.innerHTML=`Hai pareggiato! Il computer ha scelto ${computerChoice} riprova!`
+            console.log("Pareggio")
+        }else if (userChoice > computerChoice){
+            result.innerHTML=`Hai vinto! Bravo! Il computer ha scelto ${computerChoice}!`
+
+            console.log("hai vinto")
+        }else{
+            result.innerHTML=`Ops! Hai perso! Il computer ha scelto ${computerChoice} riprova!`
+
+            console.log("hai perso")
+        }
+
+       
+    }
+)
